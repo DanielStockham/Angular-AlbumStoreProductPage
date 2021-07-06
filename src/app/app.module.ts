c4b6ced81efac8a476599c6cd17d563d366f8467
daniel.stockham <daniel.stockham@niceincontact.com>
2021-07-06 10:36:06 -0400

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { Routes, RouterModule } from '@angular/router';
 
-const appRoutes: Routes[] = [
+const appRoutes: Routes = [
   {
     path: '',
     redirectTo: '/products',
